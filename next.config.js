8d904ca90a0c4d826c07d3cee1b82b59dce92148
David França <davidemanuel.cf@gmail.com>
2021-01-29 12:01:07 -0300

    test env

--- next.config.js
+++ next.config.js
@@ -1,4 +1,15 @@
-const withImages = require('next-images')
+const withImages = require('next-images');
 module.exports = withImages({
   esModule: true
 })
+
+module.exports = {
+  webpack: (config, {isServer}) => {
+    if (!isServer) {
+      config.node = {
+        fs: 'empty'
+      }
+    }
+    return config
+  }
+};
--- src/config/config.ts
+++ src/config/config.ts
@@ -1,4 +1,9 @@
-import 'dotenv/config'
+import dotenv from 'dotenv/'
+if (process.env.NODE_ENV !== 'production') {
+  dotenv.config({
+    path: process.env.NODE_ENV === 'test' ? '.env.local' : '.env'
+  })
+}
 
 export default {
   mapboxKey: process.env.MAPBOX_API_KEY
