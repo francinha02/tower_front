9370bcfca00e6b032105b543392b03ad1abf1a0d
David França <davidemanuel.cf@gmail.com>
2021-01-29 13:59:40 -0300

    Remove dotenv

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,11 +1,3 @@
-import dotenv from 'dotenv/'
-if (process.env.NODE_ENV !== 'production') {
-  dotenv.config({
-    path:
-      process.env.NODE_ENV === 'development' ? '.env.development.local' : '.env'
-  })
-}
-
 export default {
   mapboxKey:
     process.env.NEXT_PUBLIC_MAPBOX_API_KEY || process.env.MAPBOX_API_KEY
